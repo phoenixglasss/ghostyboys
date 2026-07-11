297984cdadbd90069f93de97be64f2c3a4384f5c
phoenixglasss <gilted.lavendel@gmail.com>
2026-07-11 15:49:08 -0500

    state machine wired up through resolve, onto enemy turns now!

--- ghosty-boys/battle/battle_scene.gd
+++ ghosty-boys/battle/battle_scene.gd
@@ -12,9 +12,11 @@ var current_state: State = State.INTRO
 var acting_member_index: int = 0
 var pending_attack: AttackData
 var pending_result: Dictionary
+var enemy_instances: Array[Dictionary] = []
 
 func _ready() -> void:
 	action_menu.action_chosen.connect(_on_action_chosen)
+	_setup_enemies()
 	_enter_state(State.INTRO)
 	
 func _on_action_chosen(attack: AttackData) -> void:
@@ -35,8 +37,39 @@ func _enter_state(state: State) -> void:
 			pending_result = await RhythmMinigame.challenge_completed
 			_enter_state(State.RESOLVE)
 		State.RESOLVE:
-			print("Got rhythm result: ", pending_result.percentage)
+			_resolve_action()
+			_after_resolve()
 		State.ENEMY_TURN:
 			pass
 		State.CHECK_END:
 			pass
+
+func _setup_enemies() -> void:
+	for i in enemies.size():
+		enemy_instances.append({
+			"data": enemies[i],
+			"current_hp": enemies[i].max_hp,
+			"id": "enemy_%d" % i
+		})
+
+# damage math portion
+func _resolve_action() -> void:
+	var amount := roundi(pending_attack.base_power * pending_result.percentage)
+	
+	if pending_attack.is_healing:
+		var target: PartyMember = party[acting_member_index]
+		target.current_hp = min(target.current_hp + amount, target.max_hp)
+		print(target.member_name, " healed for ", amount, " -> ", target.current_hp, "/", target.max_hp)
+	else:
+		var target: Dictionary = enemy_instances[0]
+		target.current_hp = max(target.current_hp - amount, 0)
+		print(target.data.enemy_name, " took ", amount, " damage -> ", target.current_hp, "/", target.data.max_hp)
+		
+	
+func _after_resolve() -> void:
+	acting_member_index += 1
+	if acting_member_index < party.size():
+		_enter_state(State.PLAYER_MENU)
+	else:
+		acting_member_index = 0
+		_enter_state(State.ENEMY_TURN)
