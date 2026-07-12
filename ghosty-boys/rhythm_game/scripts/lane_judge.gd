2642d18fcb8adef75dbcb6a5c07b7790d68d0d94
deandimarzo <deandimarzo@gmail.com>
2026-07-12 11:07:32 -0500

    fixed error with noteitem type being cast to from area2d whoopsie

--- ghosty-boys/rhythm_game/scripts/lane_judge.gd
+++ ghosty-boys/rhythm_game/scripts/lane_judge.gd
@@ -1,20 +1,26 @@
 extends Node2D
 
 
-func _on_area_okay_area_entered(note: NoteItem) -> void:
-	note.rating = NoteItem.Rating.OKAY
+func _on_area_okay_area_entered(note: Node2D) -> void:
+	if note is NoteItem:
+		note.rating = NoteItem.Rating.OKAY
 
-func _on_area_okay_area_exited(note: NoteItem) -> void:
-	note.rating = NoteItem.Rating.MISS
+func _on_area_okay_area_exited(note: Node2D) -> void:
+	if note is NoteItem:
+		note.rating = NoteItem.Rating.MISS
 
-func _on_area_good_area_entered(note: NoteItem) -> void:
-	note.rating = NoteItem.Rating.GOOD
+func _on_area_good_area_entered(note: Node2D) -> void:
+	if note is NoteItem:
+		note.rating = NoteItem.Rating.GOOD
 
-func _on_area_good_area_exited(note: NoteItem) -> void:
-	note.rating = NoteItem.Rating.OKAY
+func _on_area_good_area_exited(note: Node2D) -> void:
+	if note is NoteItem:
+		note.rating = NoteItem.Rating.OKAY
 
-func _on_area_perfect_area_entered(note: NoteItem) -> void:
-	note.rating = NoteItem.Rating.PERFECT
+func _on_area_perfect_area_entered(note: Node2D) -> void:
+	if note is NoteItem:
+		note.rating = NoteItem.Rating.PERFECT
 
-func _on_area_perfect_area_exited(note: NoteItem) -> void:
-	note.rating = NoteItem.Rating.GOOD
+func _on_area_perfect_area_exited(note: Node2D) -> void:
+	if note is NoteItem:
+		note.rating = NoteItem.Rating.GOOD
