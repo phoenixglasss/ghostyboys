f3189b7e080330065bcceb2967837250d6b815e6
deandimarzo <deandimarzo@gmail.com>
2026-07-16 10:16:53 -0500

    enemies also now bouncy

--- ghosty-boys/battle/enemy_display.gd
+++ ghosty-boys/battle/enemy_display.gd
@@ -2,15 +2,23 @@ extends Node2D
 class_name EnemyDisplay
 
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-@onready var placeholder: Sprite2D = $Placeholder
+var conductor : Conductor
+var bounce_curve : Curve = preload("res://rhythm_game/bounce.tres")
 
 
 func setup(enemy_data: EnemyData) -> void:
 	if enemy_data.sprite_frames:
-		placeholder.visible = true
-		sprite.visible = true
 		sprite.sprite_frames = enemy_data.sprite_frames
-		sprite.play("idle")
-	else:
-		placeholder.visible = true
-		sprite.visible = true
+	sprite.play("idle")
+	
+	var current_texture: Texture2D = sprite.sprite_frames.get_frame_texture(sprite.animation, 0)
+	var texture_height: float = current_texture.get_size().y
+	var current_height: float = texture_height * scale.y
+	sprite.offset.y -= current_height
+	sprite.position.y += current_height
+
+func _process(delta: float) -> void:
+	if conductor:
+		var bounded_beat : float = fmod(conductor.raw_beat,2.0)
+		var bounce_scale_mod = bounce_curve.sample_baked(bounded_beat)
+		sprite.scale.y = 1 + bounce_scale_mod * 0.05
