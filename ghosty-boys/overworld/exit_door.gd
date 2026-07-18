37436188e8de0d585a87700e8ad2bbf50a1975d6
phoenixglasss <gilted.lavendel@gmail.com>
2026-07-17 19:57:01 -0500

    door blocked until intro/tutorial complete

---
+++ ghosty-boys/overworld/exit_door.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+@export var blocked_conversation: DialogueConversation
+@export var destination_scene: String
+
+var has_triggered: bool = false
+
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+	body_exited.connect(_on_body_exited)
+	
+	
+func _on_body_entered(body: Node2D) -> void:
+	if not body.is_in_group("player"):
+		return
+		
+	if GameState.bar_unlocked:
+		if has_triggered:
+			return
+		has_triggered = true
+		SceneTransition.fade_to_scene(destination_scene)
+	else:
+		if not DialogueBox.visible:
+			DialogueBox.start_conversation(blocked_conversation)
+		
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		has_triggered = false
